916c6a2dbf17eb44a58dfaaf86a15700ada248f2
Dalton Pyle <dalton.w.pyle@ou.edu>
2019-11-21 00:14:04 -0600

    cleaned up the code and moded things to different methods

--- HammingDistanceCalculator.java
+++ HammingDistanceCalculator.java
@@ -24,7 +24,35 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class HammingDistanceCalculator extends Application {
-
+	TextField distance0;
+	TextField distance1;
+	TextField distance2;
+	TextField distance3;
+	TextField distance4;
+	TextField hammingDistance;
+	TextField addStationsTxt;
+	
+	TextArea StationsBox;
+	
+	Label hammingDistanceLabel;
+	Label distanceLablel0;
+	Label distanceLablel1;
+	Label distanceLablel2;
+	Label distanceLablel3;
+	Label distanceLablel4;
+	Label compareWithLabel;
+	
+	Button showStation;
+	Button calculateHD;
+	Button addStation;
+	ComboBox<String> compareWithDropBox;
+	
+	Slider hammingValue;
+	GridPane gridPane;
+	
+	Scene scene;
+	
+	ArrayList<String> stations;
 	public static void main(String[] args) {
 		launch(args);
 		
@@ -33,141 +61,56 @@ public class HammingDistanceCalculator extends Application {
 	
 	@Override
 	public void start(Stage applicationStage) throws Exception {
-		GridPane gridPane = new GridPane();
-		Scene scene = new Scene(gridPane);
+	
+		initilizeStationsList();
+		initizeTextField();
 		
-		TextField distance0 = new TextField();
-		distance0.setEditable(false);
-		TextField distance1 = new TextField();
-		distance1.setEditable(false);
-		TextField distance2 = new TextField();
-		distance2.setEditable(false);
-		TextField distance3 = new TextField();
-		distance3.setEditable(false);
-		TextField distance4 = new TextField();
-		distance4.setEditable(false);
-		TextField hammingDistance = new TextField();
-		hammingDistance.setEditable(false);
-		TextField addStationsTxt = new TextField();
+		initilizeTextArea();
 		
-		TextArea StationsBox = new TextArea();
-		StationsBox.setEditable(false);
-		StationsBox.setPrefSize(100	,250); 
+		initilizeLabels();
 		
+		initilizeButtons();
 		
-		Label hammingDistanceLabel = new Label("Enter Hamming Dist:");
-		Label distanceLable0 = new Label("Distance0");
-		Label distanceLabel1 = new Label("Distance1");
-		Label distanceLabel2 = new Label ("Distance2");
-		Label distanceLabel3 = new Label ("Distance3");
-		Label distanceLabel4 = new Label ("Distance4");
-		Label compareWithLabel = new Label ("Compare with:");
+		initilizeComboBox();
+		
+		initilizeSlider();
+		
+		initilizePane();
 		
 		
 		
-		Button showStation = new Button("Show Station");
-		Button calculateHD = new Button("Calculate HD");
-		Button addStation = new Button ("Add Station");
-		
-		ComboBox<String> compareWithDropBox = new ComboBox<String>();
-		MesontReader MR = new MesontReader();
-		compareWithDropBox.getItems().addAll(MR.fileReader());
-		
-		Slider hammingValue = new Slider();
-		hammingValue.setMax(4);
-		hammingValue.setMin(1);
-		hammingValue.setMinorTickCount(0);
-		hammingValue.setMajorTickUnit(1);
-		hammingValue.setBlockIncrement(1);
-		
-		hammingValue.setShowTickMarks(true);
-		hammingValue.setSnapToTicks(true);
-		hammingValue.setShowTickLabels(true);
-		
-		gridPane.setColumnSpan(StationsBox, 2);
-		
-		gridPane.add(hammingDistanceLabel,0,0);
-		gridPane.add(hammingDistance,1,0);
-		gridPane.add(hammingValue, 0, 1);
-		gridPane.add(showStation, 0, 2);
-		gridPane.add(StationsBox, 0, 3);
-		gridPane.add(compareWithLabel, 0, 4);
-		gridPane.add(compareWithDropBox,1,4);
-		gridPane.add(calculateHD, 0, 5);
-		gridPane.add(distance0, 1, 7);
-		gridPane.add(distanceLable0, 0, 7);
-		gridPane.add(distance1, 1, 8);
-		gridPane.add(distanceLabel1, 0, 8);
-		gridPane.add(distance2, 1, 9);
-		gridPane.add(distanceLabel2, 0, 9);
-		gridPane.add(distance3, 1, 10);
-		gridPane.add(distanceLabel3, 0, 10);
-		gridPane.add(distance4, 1, 11);
-		gridPane.add(distanceLabel4, 0, 11);
-		gridPane.add(addStation, 0, 12);
-		gridPane.add(addStationsTxt, 1, 12);
-		
-		
-		
-		
-
-		
-		Insets gdridPadding = new Insets(10,10,10,10);
-		gridPane.setPadding(gdridPadding);
-		gridPane.setVgap(10);
-		gridPane.setHgap(10);
 		applicationStage.setScene(scene);
 		applicationStage.setTitle("Hamming Distance");
 		applicationStage.show();
 		
 		showStation.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
-			public void handle(ActionEvent event){
-				
-				try {
-					ArrayList<String> stations = MR.fileReader();
-					String comparingTo = compareWithDropBox.getValue();
-					int desieredHammingDistance = (int)hammingValue.getValue();
-					for (int i = 0; i < stations.size(); ++i) {
-						if (HammingDist.distCalculator(stations.get(i), comparingTo) == desieredHammingDistance)
-							StationsBox.setText(StationsBox.getText() + "\n" + stations.get(i));
-					}
-				} catch (IOException e) {
-				
+			public void handle(ActionEvent event) {
+
+				String comparingTo = compareWithDropBox.getValue();
+				int desieredHammingDistance = (int) hammingValue.getValue();
+				for (int i = 0; i < stations.size(); ++i) {
+					if (HammingDist.distCalculator(stations.get(i), comparingTo) == desieredHammingDistance)
+						StationsBox.setText(StationsBox.getText() + "\n" + stations.get(i));
 				}
-				
-				
+
 			}
 		});
 		calculateHD.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 				String comparingTo = compareWithDropBox.getValue();
-				try {
-					ArrayList<String> stations = MR.fileReader();
-					
-							
-									distance0.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 0)));
-									
-								
-									distance1.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 1)));
-								
-								
-									distance2.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 2)));
-								
-								
-									distance3.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 3)));
-									
-								
-									distance4.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 4)));
-								
-							
-								
-						
-					
-				} catch (IOException e) {
-					
-				}
+
+				distance0.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 0)));
+
+				distance1.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 1)));
+
+				distance2.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 2)));
+
+				distance3.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 3)));
+
+				distance4.setText(Integer.toString(HammingDist.simmilarDistance(stations, comparingTo, 4)));
+
 			}
 		});
 		addStation.setOnAction(new EventHandler<ActionEvent>() {
@@ -183,6 +126,115 @@ public class HammingDistanceCalculator extends Application {
 				hammingDistance.setText(Double.toString(currentValue));
 			}
 		});
+	}
+
+
+	private void initilizeStationsList() throws IOException {
+		MesontReader MR = new MesontReader();
+		 stations = MR.fileReader();
+		
+	}
+
+
+	private void initilizePane() {
+		 gridPane = new GridPane();
+		 scene = new Scene(gridPane);
+		
+		gridPane.setColumnSpan(StationsBox, 2);
+		
+		gridPane.add(hammingDistanceLabel,0,0);
+		gridPane.add(hammingDistance,1,0);
+		gridPane.add(hammingValue, 0, 1);
+		gridPane.add(showStation, 0, 2);
+		gridPane.add(StationsBox, 0, 3);
+		gridPane.add(compareWithLabel, 0, 4);
+		gridPane.add(compareWithDropBox,1,4);
+		gridPane.add(calculateHD, 0, 5);
+		gridPane.add(distance0, 1, 7);
+		gridPane.add(distanceLablel0, 0, 7);
+		gridPane.add(distance1, 1, 8);
+		gridPane.add(distanceLablel1, 0, 8);
+		gridPane.add(distance2, 1, 9);
+		gridPane.add(distanceLablel2, 0, 9);
+		gridPane.add(distance3, 1, 10);
+		gridPane.add(distanceLablel3, 0, 10);
+		gridPane.add(distance4, 1, 11);
+		gridPane.add(distanceLablel4, 0, 11);
+		gridPane.add(addStation, 0, 12);
+		gridPane.add(addStationsTxt, 1, 12);
+		
+		Insets gdridPadding = new Insets(10,10,10,10);
+		gridPane.setPadding(gdridPadding);
+		gridPane.setVgap(10);
+		gridPane.setHgap(10);
+	}
+
+
+	private void initilizeSlider() {
+		hammingValue = new Slider();
+		hammingValue.setMax(4);
+		hammingValue.setMin(1);
+		hammingValue.setMinorTickCount(0);
+		hammingValue.setMajorTickUnit(1);
+		hammingValue.setBlockIncrement(1);
+		
+		hammingValue.setShowTickMarks(true);
+		hammingValue.setSnapToTicks(true);
+		hammingValue.setShowTickLabels(true);
+		
+	}
+
+
+	private void initilizeComboBox() {
+		compareWithDropBox = new ComboBox<String>();
+		compareWithDropBox.getItems().addAll(stations);
+		
+	}
+
+
+	private void initilizeButtons() {
+		 showStation = new Button("Show Station");
+		 calculateHD = new Button("Calculate HD");
+		 addStation = new Button ("Add Station");
+		
+	}
+
+
+	private void initilizeLabels() {
+		 hammingDistanceLabel = new Label("Enter Hamming Dist:");
+		 distanceLablel0 = new Label("Distance0");
+		 distanceLablel1 = new Label("Distance1");
+		 distanceLablel2 = new Label ("Distance2");
+		 distanceLablel3 = new Label ("Distance3");
+		 distanceLablel4 = new Label ("Distance4");
+		 compareWithLabel = new Label ("Compare with:");
+		
+	}
+
+
+	private void initilizeTextArea() {
+		StationsBox = new TextArea();
+		StationsBox.setEditable(false);
+		StationsBox.setPrefSize(100	,250); 
+		
+	}
+
+
+	private void initizeTextField() {
+		 distance0 = new TextField();
+		distance0.setEditable(false);
+		 distance1 = new TextField();
+		distance1.setEditable(false);
+		 distance2 = new TextField();
+		distance2.setEditable(false);
+		 distance3 = new TextField();
+		distance3.setEditable(false);
+		 distance4 = new TextField();
+		distance4.setEditable(false);
+		 hammingDistance = new TextField();
+		hammingDistance.setEditable(false);
+		 addStationsTxt = new TextField();
+		
 	}
 	
 	
